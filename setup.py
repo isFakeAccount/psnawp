cda05c43dd0ab8a733057c8a1bdabb759370779d
isFakeAccount <trevorphillips@gmx.us>
2021-02-26 00:58:28 -0500

    Bumped the version number and added more links

--- setup.py
+++ setup.py
@@ -9,14 +9,21 @@ classifiers = [
 
 setup(
     name='PSNAWP',
-    version='2021.20.2',
+    version='2021.26.2',
     packages=find_packages(),
+    project_urls={
+        "Change Log": "https://github.com/isFakeAccount/psnawp/commits/",
+        "Documentation": "https://psnawp.readthedocs.io/en/latest/",
+        "Issue Tracker": "https://github.com/isFakeAccount/psnawp/issues",
+        "Source Code": "https://github.com/isFakeAccount/psnawp",
+    },
     url='https://github.com/isFakeAccount/psnawp',
     license='MIT',
     author='isFakeAccount',
     author_email='trevorphillips@gmx.us',
     description='Retrieve User Information, Trophies, Game and Store data from the PlayStation Network',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     keywords='PSN API',
     classifiers=classifiers
 )
