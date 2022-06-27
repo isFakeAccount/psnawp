c9dea8a352ae143b62733a3a019ec1719577a480
isFakeAccount <trevorphillips@gmx.us>
2022-06-27 19:06:12 -0400

    Added pytest-vcr to make pytest compatible with vcrpy

--- tests/unit_tests/psnawp_client_test.py
+++ tests/unit_tests/psnawp_client_test.py
@@ -8,23 +8,27 @@ import psnawp_api
 from tests.unit_tests import my_vcr
 
 
+@pytest.mark.vcr()
 def test__authentication():
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         psnawp_api.psnawp.PSNAWP(os.getenv("NPSSO_CODE"))
 
 
+@pytest.mark.vcr()
 def test__online_id(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         client = psnawp_fixture.me()
         assert client.get_online_id() == os.getenv("USER_NAME")
 
 
+@pytest.mark.vcr()
 def test__account_id(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         client = psnawp_fixture.me()
         assert re.match(r"\d{19}", client.get_account_id())
 
 
+@pytest.mark.vcr()
 @pytest.mark.skip(reason="Not implemented yet")
 def test__account_devices(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
@@ -32,12 +36,14 @@ def test__account_devices(psnawp_fixture):
         client.get_account_devices()
 
 
+@pytest.mark.vcr()
 def test__get_friends(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         client = psnawp_fixture.me()
         client.friends_list()
 
 
+@pytest.mark.vcr()
 def test__blocked_list(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         client = psnawp_fixture.me()
