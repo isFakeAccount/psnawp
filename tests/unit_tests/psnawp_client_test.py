1dc0fd7e4ef9ec1b14b45199b8053adf37fa5ac6
isFakeAccount <trevorphillips@gmx.us>
2022-06-27 03:33:19 -0400

    Removed the fixture since it was causing issues

--- tests/unit_tests/psnawp_client_test.py
+++ tests/unit_tests/psnawp_client_test.py
@@ -8,37 +8,37 @@ import psnawp_api
 from tests.unit_tests import my_vcr
 
 
-def test__authentication():
+def test__online_id():
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
-        psnawp_api.psnawp.PSNAWP(os.getenv("NPSSO_CODE"))
-
-
-def test__online_id(psnawp_fixture):
-    with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
-        client = psnawp_fixture.me()
+        psnawp = psnawp_api.psnawp.PSNAWP(os.getenv("NPSSO_CODE"))
+        client = psnawp.me()
         assert client.get_online_id() == os.getenv("USER_NAME")
 
 
-def test__account_id(psnawp_fixture):
+def test__account_id():
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
-        client = psnawp_fixture.me()
+        psnawp = psnawp_api.psnawp.PSNAWP(os.getenv("NPSSO_CODE"))
+        client = psnawp.me()
         assert re.match(r"\d{19}", client.get_account_id())
 
 
 @pytest.mark.skip(reason="Not implemented yet")
-def test__account_devices(psnawp_fixture):
+def test__account_devices():
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
-        client = psnawp_fixture.me()
+        psnawp = psnawp_api.psnawp.PSNAWP(os.getenv("NPSSO_CODE"))
+        client = psnawp.me()
         client.get_account_devices()
 
 
-def test__get_friends(psnawp_fixture):
+def test__get_friends():
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
-        client = psnawp_fixture.me()
+        psnawp = psnawp_api.psnawp.PSNAWP(os.getenv("NPSSO_CODE"))
+        client = psnawp.me()
         client.friends_list()
 
 
-def test__blocked_list(psnawp_fixture):
+def test__blocked_list():
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
-        client = psnawp_fixture.me()
+        psnawp = psnawp_api.psnawp.PSNAWP(os.getenv("NPSSO_CODE"))
+        client = psnawp.me()
         client.blocked_list()
