f96d2bc27f24caf106fa0998ca6d595fce503fce
isFakeAccount <trevorphillips@gmx.us>
2022-09-17 01:14:39 -0400

    Updated the docs settings

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,6 @@ import os
 import sys
 from datetime import datetime
 
-sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(1, os.path.abspath("../src"))
 
 # -- Project information -----------------------------------------------------
