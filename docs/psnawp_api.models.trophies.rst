486954e9df51e4248dec729aac24eeb3ee7c9ab3
isFakeAccount <trevorphillips@gmx.us>
2022-11-03 17:24:15 -0400

    Added trophy class for future use
    Signed-off-by: isFakeAccount <trevorphillips@gmx.us>

--- docs/psnawp_api.models.trophies.rst
+++ docs/psnawp_api.models.trophies.rst
@@ -4,6 +4,14 @@ psnawp\_api.models.trophies package
 Submodules
 ----------
 
+psnawp\_api.models.trophies.trophy module
+-----------------------------------------
+
+.. automodule:: psnawp_api.models.trophies.trophy
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
 psnawp\_api.models.trophies.trophy\_set module
 ----------------------------------------------
 
---
+++ src/psnawp_api/models/trophies/trophy.py
@@ -0,0 +1,33 @@
+import attr
+from enum import Enum
+
+
+class TrophyType(Enum):
+    BRONZE = 'bronze'
+    SILVER = 'silver'
+    GOLD = 'gold'
+    PLATINUM = 'platinum'
+
+
+@attr.s(slots=True)
+class Trophy:
+    trophy_id: int
+    "Unique ID for this trophy"
+    trophy_hidden: bool
+    "True if this is a secret trophy (Only for client)"
+    trophy_type: TrophyType
+    "Type of the trophy"
+    trophy_name: str
+    "Name of trophy"
+    trophy_detail: str
+    "Description of the trophy"
+    trophy_icon_url: str
+    "URL for the graphic associated with the trophy"
+    trophy_group_id: str
+    "ID of the trophy group this trophy belongs to"
+    trophy_Progress_value: int
+    "Trophy progress towards it being unlocked (PS5 Only)"
+    trophy_reward_name: str
+    "Name of the reward earning the trophy grants (PS5 Only)"
+    trophy_reward_img_url: str
+    "URL for the graphic associated with the reward (PS5 Only)"
