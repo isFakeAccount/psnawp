04b01154abcb78837cb89e4aa12f5b27fb793291
isFakeAccount <trevorphillips@gmx.us>
2023-04-02 02:07:17 -0500

    Updated conftest.py
    Signed-off-by: isFakeAccount <trevorphillips@gmx.us>

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,4 +10,10 @@ load_dotenv()
 
 @pytest.fixture(scope="session")
 def psnawp_fixture():
+    """Creates PSNAWP Instance Fixture for running all the units tests.
+
+    :returns: PSNAWP Instance
+    :rtype: PSNAWP
+
+    """
     return PSNAWP(os.getenv("NPSSO_CODE"))
