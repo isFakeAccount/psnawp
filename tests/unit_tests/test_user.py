7c31e643ddb96bbeedfb4b150a5b1d55ec10bfcc
Yoshikage Kira <8bit_yoshikage_kira@proton.me>
2024-05-19 23:57:50 -0400

    Fixes issues with unit test
    - Fixes the issues with the test case for HTTP 403, which weren't working because the user being tested didn't have any trophies and was throwing a different error.

--- tests/unit_tests/test_user.py
+++ tests/unit_tests/test_user.py
@@ -114,7 +114,7 @@ def test_user__trophy_summary(psnawp_fixture):
 def test_user__trophy_summary_forbidden(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         with pytest.raises(PSNAWPForbidden):
-            psnawp_fixture.user(online_id="isFakeAccount").trophy_summary()
+            psnawp_fixture.user(online_id="test").trophy_summary()
 
 
 @pytest.mark.vcr()
@@ -137,7 +137,7 @@ def test_user__trophy_titles(psnawp_fixture):
 def test_user__trophy_titles_forbidden(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         with pytest.raises(PSNAWPForbidden):
-            for trophy_title in psnawp_fixture.user(online_id="kerksten").trophy_titles(limit=100):
+            for trophy_title in psnawp_fixture.user(online_id="test").trophy_titles(limit=100):
                 print(trophy_title)
 
 
@@ -182,7 +182,7 @@ def test_user__trophy_titles_for_title(psnawp_fixture):
 def test_user__trophy_titles_for_title_forbidden(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         with pytest.raises(PSNAWPForbidden):
-            for trophy_title in psnawp_fixture.user(online_id="isFakeAccount").trophy_titles_for_title(title_ids=["CUSA12057_00"]):
+            for trophy_title in psnawp_fixture.user(online_id="test").trophy_titles_for_title(title_ids=["CUSA12057_00"]):
                 print(trophy_title)
 
 
@@ -210,10 +210,10 @@ def test_user__trophies(psnawp_fixture):
 def test_user__trophies_forbidden(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         with pytest.raises(PSNAWPForbidden):
-            list(psnawp_fixture.user(online_id="isFakeAccount").trophies("NPWR15509_00", "PS4", limit=10))
+            list(psnawp_fixture.user(online_id="test").trophies("NPWR15509_00", "PS4", limit=10))
 
         with pytest.raises(PSNAWPForbidden):
-            list(psnawp_fixture.user(online_id="isFakeAccount").trophies("NPWR15509_00", "PS4", limit=10, include_metadata=True))
+            list(psnawp_fixture.user(online_id="test").trophies("NPWR15509_00", "PS4", limit=10, include_metadata=True))
 
 
 @pytest.mark.vcr()
@@ -259,10 +259,10 @@ def test_user__trophy_groups_summary(psnawp_fixture):
 def test_user__trophy_groups_summary_forbidden(psnawp_fixture):
     with my_vcr.use_cassette(f"{inspect.currentframe().f_code.co_name}.yaml"):
         with pytest.raises(PSNAWPForbidden):
-            psnawp_fixture.user(online_id="isFakeAccount").trophy_groups_summary("NPWR15509_00", "PS4")
+            psnawp_fixture.user(online_id="test").trophy_groups_summary("NPWR15509_00", "PS4")
 
         with pytest.raises(PSNAWPForbidden):
-            psnawp_fixture.user(online_id="isFakeAccount").trophy_groups_summary("NPWR15509_00", "PS4", include_metadata=True)
+            psnawp_fixture.user(online_id="test").trophy_groups_summary("NPWR15509_00", "PS4", include_metadata=True)
 
 
 @pytest.mark.vcr()
