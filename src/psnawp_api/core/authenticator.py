054987f18ffd3dfb67981fd2a801af2322c8e0d4
Yoshikage Kira <trevorphillips@gmx.us>
2024-05-28 01:49:41 -0400

    Fixes the OAuth Method in Authentication
    - Fixes the OAuth Method and makes it closer to Android App version
    - Adds new params to Authorization method as well to bring it closer to how app behaves
    - Renames auth_properties to better name and add TypeDict to make it more robust

--- src/psnawp_api/core/authenticator.py
+++ src/psnawp_api/core/authenticator.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import time
+import uuid
 from functools import wraps
 from logging import getLogger
-from typing import Any, Callable, TypeVar, cast
+from typing import Callable, NotRequired, Optional, TypedDict, TypeVar, cast
 from urllib.parse import parse_qs, urlparse
 
 from requests import Response
@@ -21,7 +22,7 @@ def pre_request_processing(method: Callable[PT, RT]) -> Callable[PT, RT]:
     @wraps(method)
     def _impl(*method_args: PT.args, **method_kwargs: PT.kwargs) -> RT:
         authenticator_obj = cast(Authenticator, method_args[0])
-        if not authenticator_obj.auth_properties:
+        if not authenticator_obj.token_response:
             authorization_code = authenticator_obj.get_authorization_code()
             authenticator_obj.fetch_access_token_from_authorization(authorization_code)
         else:
@@ -36,10 +37,22 @@ def pre_request_processing(method: Callable[PT, RT]) -> Callable[PT, RT]:
 authenticator_logger = getLogger("psnawp")
 
 
+class TokenResponse(TypedDict):
+    access_token_expires_at: NotRequired[float]
+    access_token: str
+    expires_in: int
+    id_token: str
+    refresh_token_expires_at: NotRequired[float]
+    refresh_token_expires_in: int
+    refresh_token: str
+    scope: str
+    token_type: str
+
+
 class Authenticator:
     """Provides an interface for PSN Authentication and API"""
 
-    __PARAMS = {
+    __CONSTANTS = {
         "CLIENT_ID": "09515159-7237-4370-9b40-3806e67c0891",
         "SCOPE": "psn:mobile.v2.core psn:clientapp",
         "REDIRECT_URI": "com.scee.psxandroid.scecompcall://redirect",
@@ -62,46 +75,63 @@ class Authenticator:
         self.npsso_token = npsso_cookie
         self.common_headers = common_headers
         self.request_builder = RequestBuilder(common_headers)
-        self.auth_properties: dict[str, Any] = {}
+        self.token_response: Optional[TokenResponse] = None
+
+        self.cid = str(uuid.uuid4())
 
     @property
     def access_token_expiration_time(self) -> float:
-        return cast(float, self.auth_properties.get("access_token_expires_at", time.time()))
+        if self.token_response is None:
+            return time.time()
+        return self.token_response.get("access_token_expires_at", time.time())
 
     @property
     def refresh_token_expiration_time(self) -> float:
-        return cast(float, self.auth_properties.get("refresh_token_expires_at", time.time()))
+        if self.token_response is None:
+            return time.time()
+        return self.token_response.get("refresh_token_expires_at", time.time())
 
     @property
     def access_token_expiration_in(self) -> int:
-        return cast(int, self.auth_properties.get("expire_in", 0))
+        if self.token_response is None:
+            return 0
+        return self.token_response.get("expires_in", 0)
 
     @property
     def refresh_token_expiration_in(self) -> int:
-        return cast(int, self.auth_properties.get("refresh_token_expires_in", 0))
+        if self.token_response is None:
+            return 0
+        return self.token_response.get("refresh_token_expires_in", 0)
 
     def fetch_access_token_from_refresh(self) -> None:
         """Obtain the access token using refresh token."""
 
-        refresh_token = self.auth_properties["refresh_token"]
+        if self.token_response is None:
+            raise PSNAWPAuthenticationError("Attempt to obtain access_token using refresh token when refresh token is missing.")
+
+        refresh_token = self.token_response["refresh_token"]
 
         if self.access_token_expiration_time > time.time():
             return None
 
+        header = type(self).__AUTH_HEADER | {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "com.sony.snei.np.android.sso.share.oauth.versa.USER_AGENT",
+        }
         data = {
             "refresh_token": refresh_token,
             "grant_type": "refresh_token",
-            "scope": type(self).__PARAMS["SCOPE"],
+            "scope": type(self).__CONSTANTS["SCOPE"],
             "token_format": "jwt",
         }
         response = self.request_builder.post(
             url=f"{BASE_PATH['base_uri']}{API_PATH['access_token']}",
-            headers=type(self).__AUTH_HEADER,
+            headers=header,
             data=data,
         )
-        self.auth_properties = response.json()
-        self.auth_properties["access_token_expires_at"] = self.auth_properties["expires_in"] + time.time()
-        self.auth_properties["refresh_token_expires_at"] = self.auth_properties["refresh_token_expires_in"] + time.time()
+        self.token_response = cast(TokenResponse, response.json())
+        self.token_response["access_token_expires_at"] = self.token_response["expires_in"] + time.time()
+        self.token_response["refresh_token_expires_at"] = self.token_response["refresh_token_expires_in"] + time.time()
 
     def fetch_access_token_from_authorization(self, authorization_code: str) -> None:
         """Obtain the access token using authorization code for the first time, after this the access token is obtained via refresh token.
@@ -110,22 +140,29 @@ class Authenticator:
 
         """
 
+        header = type(self).__AUTH_HEADER | {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "com.sony.snei.np.android.sso.share.oauth.versa.USER_AGENT",
+            "X-Psn-Correlation-Id": self.cid,
+        }
+
         data = {
+            "cid": self.cid,
             "code": authorization_code,
             "grant_type": "authorization_code",
-            "redirect_uri": type(self).__PARAMS["REDIRECT_URI"],
-            "scope": type(self).__PARAMS["SCOPE"],
+            "redirect_uri": type(self).__CONSTANTS["REDIRECT_URI"],
+            "scope": type(self).__CONSTANTS["SCOPE"],
             "token_format": "jwt",
         }
 
         response = self.request_builder.post(
             url=f"{BASE_PATH['base_uri']}{API_PATH['access_token']}",
-            headers=type(self).__AUTH_HEADER,
+            headers=header,
             data=data,
         )
-        self.auth_properties = response.json()
-        self.auth_properties["access_token_expires_at"] = self.auth_properties["expires_in"] + time.time()
-        self.auth_properties["refresh_token_expires_at"] = self.auth_properties["refresh_token_expires_in"] + time.time()
+        self.token_response = cast(TokenResponse, response.json())
+        self.token_response["access_token_expires_at"] = self.token_response["expires_in"] + time.time()
+        self.token_response["refresh_token_expires_at"] = self.token_response["refresh_token_expires_in"] + time.time()
 
     def get_authorization_code(self) -> str:
         """Obtains the authorization code for PSN authentication.
@@ -136,13 +173,34 @@ class Authenticator:
         :raises: ``PSNAWPAuthenticationError`` If authorization is not successful.
 
         """
-        headers = {"Cookie": f"npsso={self.npsso_token}"}
+        headers = {
+            "Cookie": f"npsso={self.npsso_token}",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-Requested-With": "com.scee.psxandroid",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-site",
+            "Sec-Fetch-User": "?1",
+        }
         params = {
             "access_type": "offline",
-            "client_id": type(self).__PARAMS["CLIENT_ID"],
-            "scope": type(self).__PARAMS["SCOPE"],
-            "redirect_uri": type(self).__PARAMS["REDIRECT_URI"],
+            "cid": self.cid,
+            "client_id": type(self).__CONSTANTS["CLIENT_ID"],
+            "device_base_font_size": "10",
+            "device_profile": "mobile",
+            "elements_visibility": "no_aclink",
+            "enable_scheme_error_code": "true",
+            "no_captcha": "true",
+            "PlatformPrivacyWs1": "minimal",
+            "redirect_uri": type(self).__CONSTANTS["REDIRECT_URI"],
             "response_type": "code",
+            "scope": type(self).__CONSTANTS["SCOPE"],
+            "service_entity": "urn:service-entity:psn",
+            "service_logo": "ps",
+            "smcid": "psapp:signin",
+            "support_scheme": "sneiprls",
+            "turnOnTrustedBrowser": "true",
+            "ui": "pr",
         }
         response = self.request_builder.get(
             url=f"{BASE_PATH['base_uri']}{API_PATH['oauth_code']}",
@@ -164,12 +222,18 @@ class Authenticator:
 
     @pre_request_processing
     def get(self, **kwargs: Unpack[RequestOptions]) -> Response:
+        if self.token_response is None:
+            raise PSNAWPAuthenticationError("Attempt to make HTTP Request without access_token.")
+
         if "headers" in kwargs:
-            kwargs["headers"]["Authorization"] = f"Bearer {self.auth_properties['access_token']}"
+            kwargs["headers"]["Authorization"] = f"Bearer {self.token_response['access_token']}"
         return self.request_builder.get(**kwargs)
 
     @pre_request_processing
     def post(self, **kwargs: Unpack[RequestOptions]) -> Response:
+        if self.token_response is None:
+            raise PSNAWPAuthenticationError("Attempt to make HTTP Request without access_token.")
+
         if "headers" in kwargs:
-            kwargs["headers"]["Authorization"] = f"Bearer {self.auth_properties['access_token']}"
+            kwargs["headers"]["Authorization"] = f"Bearer {self.token_response['access_token']}"
         return self.request_builder.post(**kwargs)
