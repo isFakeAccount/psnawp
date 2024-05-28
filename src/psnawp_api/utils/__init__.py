40b7133e01179f742d8e3247b60346d2d4bd8ee6
Yoshikage Kira <trevorphillips@gmx.us>
2024-05-27 23:15:01 -0400

    Removes the refresh token warn logic because of added complexity
    - Removes the refresh token warn logic because of added complexity. It makes more sense for user to do it now that there are methods to get what the want.
    - Changes param name from default_headers to common_headers to be more clear
    - Removes create_logger function from __init__.py file in utils
    - Add logic in header to call methods that will fetch new refresh token whether from

--- src/psnawp_api/utils/__init__.py
+++ src/psnawp_api/utils/__init__.py
@@ -1,4 +1,4 @@
 from psnawp_api.utils.endpoints import API_PATH, BASE_PATH
-from psnawp_api.utils.misc import create_logger, iso_format_to_datetime
+from psnawp_api.utils.misc import iso_format_to_datetime
 
-__all__ = ["BASE_PATH", "API_PATH", "iso_format_to_datetime", "create_logger"]
+__all__ = ["BASE_PATH", "API_PATH", "iso_format_to_datetime"]
