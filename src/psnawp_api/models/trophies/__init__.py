8aca0dff1417684a5efbe8f48d34ea3ff810c860
Yoshikage Kira <trevorphillips@gmx.us>
2024-06-02 22:58:17 -0400

    Add trophy-related model imports to init file
    Imported trophy-related models in the trophies package to the init file for easier access and organization.

---
+++ src/psnawp_api/models/trophies/__init__.py
@@ -0,0 +1,10 @@
+from psnawp_api.models.trophies.trophy_constants import PlatformType
+from psnawp_api.models.trophies.trophy_group import EarnedTrophyGroupSummary, TrophyGroupSummary
+from psnawp_api.models.trophies.trophy_summary import TrophySummary
+
+__all__ = [
+    "PlatformType",
+    "TrophyGroupSummary",
+    "EarnedTrophyGroupSummary",
+    "TrophySummary",
+]
