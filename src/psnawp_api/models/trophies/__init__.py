148edc17543eafd81039cc1cb12f94de3be6a397
Yoshikage Kira <trevorphillips@gmx.us>
2024-06-09 00:34:16 -0400

    Reorder TrophyGroup imports for consistency
    Changed the order of TrophyGroup imports to maintain consistency in the code structure.

--- src/psnawp_api/models/trophies/__init__.py
+++ src/psnawp_api/models/trophies/__init__.py
@@ -1,6 +1,6 @@
 from psnawp_api.models.trophies.trophy import Trophy, TrophyIterator, TrophyWithProgress, TrophyWithProgressIterator
 from psnawp_api.models.trophies.trophy_constants import PlatformType, TrophySet
-from psnawp_api.models.trophies.trophy_group import TrophyGroupSummaryWithProgress, TrophyGroupSummary
+from psnawp_api.models.trophies.trophy_group import TrophyGroupSummary, TrophyGroupSummaryWithProgress
 from psnawp_api.models.trophies.trophy_summary import TrophySummary
 from psnawp_api.models.trophies.trophy_titles import TrophyTitle, TrophyTitleIterator
 
