c95adf848a3900f2b968f84a557b93c543c5baac
Yoshikage Kira <trevorphillips@gmx.us>
2024-06-09 00:21:38 -0400

    Changes class name EarnedTrophySummary to TrophyGroupSummaryWithProgress
    Updated the class name from EarnedTrophyGroupSummary to TrophyGroupSummaryWithProgress for clearer representation and functionality.

--- src/psnawp_api/models/trophies/__init__.py
+++ src/psnawp_api/models/trophies/__init__.py
@@ -1,11 +1,11 @@
 from psnawp_api.models.trophies.trophy import Trophy, TrophyIterator, TrophyWithProgress, TrophyWithProgressIterator
 from psnawp_api.models.trophies.trophy_constants import PlatformType, TrophySet
-from psnawp_api.models.trophies.trophy_group import EarnedTrophyGroupSummary, TrophyGroupSummary
+from psnawp_api.models.trophies.trophy_group import TrophyGroupSummaryWithProgress, TrophyGroupSummary
 from psnawp_api.models.trophies.trophy_summary import TrophySummary
 from psnawp_api.models.trophies.trophy_titles import TrophyTitle, TrophyTitleIterator
 
 __all__ = [
-    "EarnedTrophyGroupSummary",
+    "TrophyGroupSummaryWithProgress",
     "PlatformType",
     "Trophy",
     "TrophyGroupSummary",
