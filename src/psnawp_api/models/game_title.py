de8a8b725a4c9bb1f1c4b126ed7d4da1f845b42e
Yoshikage Kira <trevorphillips@gmx.us>
2024-06-09 02:00:14 -0400

    Refactor game title model for improved trophy handling & authentication.
    - Updated method parameters and imports for better organization.
    - Added PaginationArguments class for more control over trophy retrieval.
    - Refactored trophy retrieval methods to use TrophyIterator for enhanced functionality.

--- src/psnawp_api/models/game_title.py
+++ src/psnawp_api/models/game_title.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
-from typing import Any, Iterator, Literal, Optional
+from typing import TYPE_CHECKING, Any, Optional
 
-from psnawp_api.core import RequestBuilder
-from psnawp_api.models.trophies.trophy import Trophy, TrophyBuilder
+from psnawp_api.models.listing import PaginationArguments
+from psnawp_api.models.trophies import TrophyIterator
 from psnawp_api.models.trophies.trophy_group import (
     TrophyGroupsSummary,
     TrophyGroupsSummaryBuilder,
 )
-from psnawp_api.models.trophies.trophy_titles import TrophyTitles
+from psnawp_api.models.trophies.trophy_titles import TrophyTitleIterator
 from psnawp_api.utils import API_PATH, BASE_PATH
+
+if TYPE_CHECKING:
+    from psnawp_api.core import Authenticator
+    from psnawp_api.models.trophies import PlatformType, TrophyGroupsSummary, TrophyGroupSummary
 
 
 class GameTitle:
@@ -21,7 +25,7 @@ class GameTitle:
 
     """
 
-    def __init__(self, request_builder: RequestBuilder, title_id: str, account_id: str, np_communication_id: Optional[str]):
+    def __init__(self, authenticator: Authenticator, title_id: str, account_id: str, np_communication_id: Optional[str]):
         """The GameTitle class constructor.
 
         .. note::
@@ -30,28 +34,25 @@ class GameTitle:
 
         .. note::
 
-            During the construction of the object, an additional call is made to get the np_communication_id. This ID is important for getting trophy data.
+            During the construction of the object, an additional call is made to get the np_communication_id. This ID is important for getting trophy data. You
+            can avoid this additional HTTP request by passing in ``np_communication_id`` if you have it already.
 
         .. note::
 
             ``title_id`` can be obtained from https://andshrew.github.io/PlayStation-Titles/ or from :py:meth:`psnawp_api.models.search.Search.get_title_id`
 
-        :param request_builder: The instance of RequestBuilder. Used to make HTTPRequests.
-        :type request_builder: RequestBuilder
+        :param authenticator: The Authenticator instance used for making authenticated requests to the API.
         :param title_id: unique id of game.
-        :type title_id: str
-        :param: account_id: The account whose trophy list is being accessed
-        :type account_id: str
+        :param: account_id: The account whose trophy list is being accessed.
         :param np_communication_id: Unique ID of a game title used to request trophy information.
-        :type np_communication_id: Optional[str]
 
-        :raises: ``PSNAWPNotFound`` If the user does not have any trophies for that game or the game doesn't exist.
+        :raises PSNAWPNotFound: If the user does not have any trophies for that game or the game doesn't exist.
 
         """
-        self._request_builder = request_builder
+        self.authenticator = authenticator
         self.title_id = title_id
         if np_communication_id is None:
-            self.np_communication_id = TrophyTitles.get_np_communication_id(request_builder, title_id, account_id)
+            self.np_communication_id = TrophyTitleIterator.get_np_communication_id(authenticator, title_id, account_id)
         else:
             self.np_communication_id = np_communication_id
 
@@ -59,16 +60,15 @@ class GameTitle:
         """Get game details such as full name, description, genre, promotional videos/images, etc...
 
         :returns: A list of dicts containing info similar to what is shown below (Not all values are shown because of space limitations):
-        :rtype: list[dict[str, Any]]
 
             .. literalinclude:: examples/game_title/get_details.json
                 :language: json
 
         """
 
-        param = {"age": 99, "country": "US", "language": "en-US"}
+        param: dict[str, int | str] = {"age": 99, "country": "US", "language": "en-US"}
 
-        response: list[dict[str, Any]] = self._request_builder.get(
+        response: list[dict[str, Any]] = self.authenticator.get(
             url=f"{BASE_PATH['game_titles']}{API_PATH['title_concept'].format(title_id=self.title_id)}",
             params=param,
         ).json()
@@ -77,46 +77,49 @@ class GameTitle:
 
     def trophies(
         self,
-        platform: Literal["PS Vita", "PS3", "PS4", "PS5"],
+        platform: PlatformType,
         trophy_group_id: str = "default",
         limit: Optional[int] = None,
-    ) -> Iterator[Trophy]:
+        offset: int = 0,
+        page_size: int = 200,
+    ) -> TrophyIterator:
         """Retrieves the individual trophy detail of a single - or all - trophy groups for a title.
 
         :param platform: The platform this title belongs to.
-        :type platform: Literal
         :param trophy_group_id: ID for the trophy group. Each game expansion is represented by a separate ID. all to return all trophies for the title, default
             for the game itself, and additional groups starting from 001 and so on return expansions trophies.
-        :type trophy_group_id: str
         :param limit: Limit of trophies returned, None means to return all trophy titles.
-        :type limit: Optional[int]
+        :param offset: The starting point within the collection of trophies.
+        :param page_size: The number of trophies to return per page.
 
         :returns: Returns the Trophy Generator object with all the information
-        :rtype: Iterator[Trophy]
 
-        :raises: ``PSNAWPNotFound`` if you don't have any trophies for that game.
+        :raises PSNAWPNotFound: If you don't have any trophies for that game.
 
         """
-        return TrophyBuilder(
-            request_builder=self._request_builder,
-            np_communication_id=self.np_communication_id,
-        ).game_trophies(platform=platform, trophy_group_id=trophy_group_id, limit=limit)
 
-    def trophy_groups_summary(self, platform: Literal["PS Vita", "PS3", "PS4", "PS5"]) -> TrophyGroupsSummary:
+        pg_args = PaginationArguments(total_limit=limit, offset=offset, page_size=page_size)
+        return TrophyIterator.from_endpoint(
+            authenticator=self.authenticator,
+            pagination_args=pg_args,
+            np_communication_id=self.np_communication_id,
+            platform=platform,
+            trophy_group_id=trophy_group_id,
+        )
+
+    def trophy_groups_summary(self, platform: PlatformType) -> TrophyGroupsSummary[TrophyGroupSummary]:
         """Retrieves the trophy groups for a title and their respective trophy count.
 
         This is most commonly seen in games which have expansions where additional trophies are added.
 
         :param platform: The platform this title belongs to.
-        :type platform: Literal
 
         :returns: TrophyGroupSummary object containing title and title groups trophy information.
-        :rtype: TrophyGroupsSummary
 
         :raises: ``PSNAWPNotFound`` if you don't have any trophies for that game.
 
         """
         return TrophyGroupsSummaryBuilder(
-            request_builder=self._request_builder,
+            authenticator=self.authenticator,
             np_communication_id=self.np_communication_id,
         ).game_title_trophy_groups_summary(platform=platform)
