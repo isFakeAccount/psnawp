50ead5863207024381d99a179c21024a1b3e7f53
Yoshikage Kira <trevorphillips@gmx.us>
2024-06-09 00:13:10 -0400

    Updates imports and __all__ list

--- src/psnawp_api/models/trophies/__init__.py
+++ src/psnawp_api/models/trophies/__init__.py
@@ -1,11 +1,19 @@
+from psnawp_api.models.trophies.trophy import Trophy, TrophyIterator, TrophyWithProgress, TrophyWithProgressIterator
 from psnawp_api.models.trophies.trophy_constants import PlatformType, TrophySet
 from psnawp_api.models.trophies.trophy_group import EarnedTrophyGroupSummary, TrophyGroupSummary
 from psnawp_api.models.trophies.trophy_summary import TrophySummary
+from psnawp_api.models.trophies.trophy_titles import TrophyTitle, TrophyTitleIterator
 
 __all__ = [
-    "PlatformType",
-    "TrophyGroupSummary",
     "EarnedTrophyGroupSummary",
-    "TrophySummary",
+    "PlatformType",
+    "Trophy",
+    "TrophyGroupSummary",
+    "TrophyIterator",
     "TrophySet",
+    "TrophySummary",
+    "TrophyTitle",
+    "TrophyTitleIterator",
+    "TrophyWithProgress",
+    "TrophyWithProgressIterator",
 ]
